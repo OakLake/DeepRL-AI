b526fce947f8e9fab73c9bdd8a4eb5e17f9cd0b6
Sammy Hasan <sammy.nm.hasan@gmail.com>
2017-12-08 07:51:02 +0000

    random search agent

---
+++ Cart-Pole-OpenAI-Gym/cart_pole_gym.py
@@ -0,0 +1,40 @@
+# Testing the OpenAI gym environment with the cart pole system
+# Sammy Hasan
+# 2017
+
+import gym
+import random
+
+class Agent():
+    '''Agent with simple proportional response'''
+    def __init__(self,obs_H,obs_L,inf_sub):
+        self.a = random.uniform(obs_L[0],obs_H[0])
+        self.b = random.uniform(-inf_sub,inf_sub) # specfic to CartPole env
+        self.c = random.uniform(obs_L[2],obs_H[2])
+        self.d = random.uniform(-inf_sub,inf_sub) # specfic to CartPole env
+    def react(self,observation):
+        reaction = self.a*observation[0] + self.b*observation[1] + self.c*observation[2] + self.d*observation[3]
+        return reaction > 0 # cart env has a discrete space if (2) pushing [0,1]:[left,righ]
+    def reflex_info(self):
+        return "[{}, {}, {}, {}]".format(self.a,self.b,self.c,self.d)
+
+
+env = gym.make('CartPole-v1')
+env.reset()
+
+observ_space_H = env.observation_space.high
+observ_space_L = env.observation_space.low
+
+
+
+for i_episode in range(42): # 42 trials
+    agent = Agent(observ_space_H,observ_space_L,10)
+    reflexes = agent.reflex_info()
+    observation = env.reset()
+    for t in range(1000):
+        env.render()
+        action = agent.react(observation)
+        observation, reward, done, info = env.step(action)
+        if done:
+            print ("Episode {} finished after {} timesteps :: {}".format(i_episode,t+1,reflexes))
+            break
