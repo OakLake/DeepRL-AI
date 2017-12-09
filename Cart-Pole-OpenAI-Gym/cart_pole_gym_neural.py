96caeb1311b99131ca51148a38d19eb12701cd0b
Sammy Hasan <sammy.nm.hasan@gmail.com>
2017-12-09 16:12:45 +0000

    fixed range ANN topology with random search

--- Cart-Pole-OpenAI-Gym/cart_pole_gym_neural.py
+++ Cart-Pole-OpenAI-Gym/cart_pole_gym_neural.py
@@ -5,6 +5,7 @@
 import gym, random
 import numpy as np
 
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 def swish(x):
@@ -13,11 +14,13 @@ def swish(x):
 class Agent():
     '''Agent with fixed neural network stucture, with swish activation function then sigmoid'''
     def __init__(self,n_struct):
+        self.nn_struct = n_struct
         self.W0 = np.random.rand(n_struct[0],n_struct[1]) - 0.5
         self.W1 = np.random.rand(n_struct[1],n_struct[2]) - 0.5
         self.W2 = np.random.rand(n_struct[2],n_struct[3]) - 0.5
     def react(self,observation):
-        in_x =  observation + [1]
+
+        in_x = np.array([observation[0],observation[1],observation[2],observation[3],1.0])
         y = np.matmul(in_x,self.W0)
         sw = swish(y)
         y = np.matmul(sw,self.W1)
@@ -28,16 +31,22 @@ class Agent():
         return reaction # cart env has a discrete space if (2) pushing [0,1]:[left,righ]
     def reflex_info(self):
         print("W0: {} \nW1: {}".format(self.W0,self.W1))
+    def nn(self):
+        return '5x{}x{}x1'.format(self.nn_struct[1],self.nn_struct[2])
 
 
 env = gym.make('CartPole-v1')
 env.reset()
 
-best_score = 0
+best_score = 100
 count = 0
 Good_agents = []
-for i_episode in range(10000000): # 42 trials
-    agent = Agent([4,7,3,1])
+
+simL = int(1e5)
+for i_episode in range(simL): # 42 trials
+    H1 = np.random.randint(5,12)
+    H2 = np.random.randint(2,5)
+    agent = Agent([5,H1,H2,1])
     # agent.reflex_info()
 
     observation = env.reset()
@@ -46,7 +55,7 @@ for i_episode in range(10000000): # 42 trials
         action = agent.react(observation)
         observation, reward, done, info = env.step(action)
         if done:
-            print ("Episode {} finished after {} timesteps".format(i_episode,t+1))
+            print ("Episode {} finished after {} timesteps ::: {}% ".format(i_episode,t+1,100*i_episode/simL))
             if (t+1) > best_score:
                 count += 1
                 best_score = t+1
@@ -65,7 +74,7 @@ for ag in Good_agents:
         action = ag.react(observation)
         observation, reward, done, info = env.step(action)
         if done:
-            print ("Finished after {} timesteps".format(t+1))
-            for _ in range(10):
+            print ("Finished after {} timesteps :: Agent NN: {}".format((t+1),ag.nn()))
+            for _ in range(20):
                 env.render()
             break
